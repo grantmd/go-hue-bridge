14bfd19e9caab1d8190a92cb3728069ed377872c
Myles Grant <myles@mylesgrant.com>
2019-11-28 15:28:31 -0800

    Working on serving up description.xml and other api endpoints

--- main.go
+++ main.go
@@ -28,13 +28,17 @@ func main() {
 	if mac == "" {
 		panic("Could not find mac address")
 	}
+	log.Printf("MAC Address: %s", mac)
 
 	mac = strings.ReplaceAll(mac, ":", "")
 	bridgeID := mac[len(mac)-6:]
+	log.Printf("Bridge ID: %s", bridgeID)
 
 	//////////////////////////////////////////////////////
 
 	router := http.NewServeMux()
+	router.Handle("/api/", serveAPI())
+	router.Handle("/description.xml", serveDescriptionXML())
 	router.Handle("/", serveIndex())
 
 	s := &http.Server{
@@ -147,6 +151,80 @@ func serveIndex() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		fmt.Println("Request:", r.URL)
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Hello, World!")
+	})
+}
+
+func serveDescriptionXML() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("Request:", r.URL)
+
+		w.Header().Set("Content-Type", "text/xml; charset=utf-8")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.WriteHeader(http.StatusOK)
+
+		xml := `<?xml version="1.0" ?>
+<root xmlns="urn:schemas-upnp-org:device-1-0">
+<specVersion><major>1</major><minor>0</minor></specVersion>
+<URLBase>http://{ip}:80/</URLBase>
+<device>
+	<deviceType>urn:schemas-upnp-org:device:Basic:1</deviceType>
+	<friendlyName>Philips hue ({ip})</friendlyName>
+	<manufacturer>Royal Philips Electronics</manufacturer>
+	<manufacturerURL>http://www.philips.com</manufacturerURL>
+	<modelDescription>Philips hue Personal Wireless Lighting</modelDescription>
+	<modelName>Philips hue bridge 2012</modelName>
+	<modelNumber>929000226503</modelNumber>
+	<modelURL>http://www.meethue.com</modelURL>
+	<serialNumber>{mac}</serialNumber>
+	<UDN>uuid:2f402f80-da50-11e1-9b23-{mac}</UDN>
+	<presentationURL>index.html</presentationURL>
+	<iconList>
+	<icon>
+		<mimetype>image/png</mimetype>
+		<height>48</height>
+		<width>48</width>
+		<depth>24</depth>
+		<url>hue_logo_0.png</url>
+	</icon>
+	<icon>
+		<mimetype>image/png</mimetype>
+		<height>120</height>
+		<width>120</width>
+		<depth>24</depth>
+		<url>hue_logo_3.png</url>
+	</icon>
+	</iconList>
+</device>
+</root>`
+
+		mac, _ := getMacAddr()
+		mac = strings.ReplaceAll(mac, ":", "")
+		mac = strings.ToLower(mac)
+
+		ip, _ := getLocalIP()
+
+		xml = strings.ReplaceAll(xml, "{mac}", mac)
+		xml = strings.ReplaceAll(xml, "{ip}", ip)
+
+		fmt.Fprintln(w, xml)
+	})
+}
+
+func serveAPI() http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("API Request:", r.URL)
+
+		// Example: /api/nouser/config
+		parts := strings.Split(r.URL.Path, "/")
+		fmt.Println(parts)
 
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
